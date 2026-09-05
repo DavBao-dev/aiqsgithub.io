305a50c0d5689ee38b5fd7f72bfa5f45ceb942ac
davbao <davidtuber710@gmail.com>
2026-09-05 16:01:02 +0700

    4th commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ====== SỬA 2 DÒNG NÀY THEO REPO CỦA BẠN ======
 window.BLOG_CONFIG = {
-  owner: "DavBao-dev",   // TODO: username GitHub của bạn
-  repo: "aiqsgithub.io",          // TODO: tên repo (vd: my-blog)
+  owner: "your-github-username",   // TODO: username GitHub của bạn
+  repo: "your-repo-name",          // TODO: tên repo (vd: my-blog)
   branch: "main",                  // nhánh deploy GitHub Pages
   postsPath: "posts",              // thư mục chứa file .md
   siteTitle: "My Blog"             // tên blog hiển thị trên header
